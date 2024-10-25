d607f814760817d5397cfb7c31de97bff57f63c9
tbm22 <tbm22@fsu.edu>
2024-10-25 14:32:11 -0400

    update power bi visualization

--- working_files/metal_music_analysis.sql
+++ working_files/metal_music_analysis.sql
@@ -1,22 +1,5 @@
 -- @block
 
-
--- get an average of the ratings where there are more then 50 reviews (for analysis integrity to prevent a potential skew)
-SELECT *
-FROM black_metal_music
-WHERE review_count > 50 AND artist = '1349'
-ORDER BY artist;
-
--- @block
-SELECT 
-    artist,
-    SUM(star_rating)
-FROM black_metal_music
-WHERE review_count > 50
-GROUP BY artist;
-
--- @block
-
 -- separate cte to query the average stars for every album released by artist
 WITH average_ratings AS (
     SELECT
